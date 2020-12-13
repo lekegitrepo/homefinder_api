01b70d285c0a94c469fc671705e9cf9b32549171
lekegitrepo <lekedevrepo@gmail.com>
2020-12-13 09:36:03 -0600

    migrate favourite table to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_151038) do
+ActiveRecord::Schema.define(version: 2020_12_13_151824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "home_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["home_id"], name: "index_favourites_on_home_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string "name"
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 2020_12_13_151038) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "homes"
+  add_foreign_key "favourites", "users"
 end
