3180e6594a4798800ea8b65da3282ff2c60e14de
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 14:58:01 -0600

    add routes for the favourites homes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
       resources :users, only: [:show, :update, :destroy]
       resources :homes, only: [:show, :index]
+      resources :favourites, only: [:index]
     end
   end
  end
