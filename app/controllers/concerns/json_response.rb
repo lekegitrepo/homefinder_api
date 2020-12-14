cde5550d471c8d1d1b669fc34502192852361a7c
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 11:55:52 -0600

    create home_json method

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -6,4 +6,12 @@ module JsonResponse
       is_success: success
     }, status: status, location: loc
   end
+
+  def home_json(msg, success, user, status, loc = nil)
+    render json: {
+      home: user,
+      message: msg,
+      is_success: success
+    }, status: status, location: loc
+  end
 end
