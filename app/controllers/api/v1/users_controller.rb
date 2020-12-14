2a8d05102e188f99a7efb48ea96c0187f1e5b445
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 05:35:27 -0600

    create user update action method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    user = current_user
+    if user.update(user_params)
+      render_json 'Successfully update user Profile', true, user, :accepted, [:api_v1, user]
+    else
+      render_json 'Unable to update user Profile', false, { errors: user.errors }, :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
