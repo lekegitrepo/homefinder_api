05ef1d573e48456cc8a056d799fd82e5f992fc89
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 05:49:47 -0600

    add user sign in specs

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SessionsController, type: :controller do
+  describe 'POST #create' do
+    before(:each) do
+      @user = FactoryBot.create :user
+    end
+
+    context 'When user provide right credentials to sign in' do
+      before(:each) do
+        credentials = { email: @user.email, password: '123456789' }
+        post :create, params: { user: credentials }
+      end
+
+      it 'returns the user record corresponding to the given credentials' do
+        @user.reload
+        user_obj = json_response[:user]
+        expect(user_obj[:auth_token]).to eql @user.auth_token
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'When user provide incorrect credentials to sign in' do
+      before(:each) do
+        credentials = { email: @user.email, password: 'wrong_password' }
+        post :create, params: { user: credentials }
+      end
+
+      it 'response with is_success equal to false' do
+        expect(json_response[:is_success]).to eql false
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
