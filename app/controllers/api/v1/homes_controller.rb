1af7e57a8b013aa3fba880a9b741ddb206cfbf66
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 11:57:32 -0600

    create the show action method in the home controller

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::HomesController < ApplicationController
+
+  def show
+    house = Home.find_by(id: params[:id])
+    if house
+      home_json 'House Detail', true, house, :ok
+    else
+      home_json 'Unable to find house information', true, { home: house.errors }, :not_found
+    end
+  end
 end
