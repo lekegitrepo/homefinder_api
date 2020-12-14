8b6402237a7a5d12f42f47aa33b92376e098ff5f
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 05:08:37 -0600

    create json response module

---
+++ app/controllers/concerns/json_response.rb
@@ -0,0 +1,9 @@
+module JsonResponse
+  def render_json(msg, success, user, status, loc = nil)
+    render json: {
+      user: user,
+      message: msg,
+      is_success: success
+    }, status: status, location: loc
+  end
+end
