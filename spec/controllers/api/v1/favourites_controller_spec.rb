86d44f98d059542e0b7f72b3c39482db64260e23
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 14:19:56 -0600

    create spec for favourites home list

--- spec/controllers/api/v1/favourites_controller_spec.rb
+++ spec/controllers/api/v1/favourites_controller_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::FavouritesController, type: :controller do
+  describe 'GET #index' do
+    before(:each) do
+      @user = FactoryBot.create :user
+      @home_list = FactoryBot.create_list(:home, 8)
+      @fav_list = FactoryBot.build_list(:favourite, 5, user_id: @user.id) do |i, idx|
+        i.id = idx
+        i.home_id = @home_list[idx].id
+      end
+      p @fav_list
+      get :index, params: { favourites: @fav_list, format: :json }
+    end
+
+    it 'should list the favourites homes' do
+    end
+
+    it { should respond_with 200 }
+  end
 end
