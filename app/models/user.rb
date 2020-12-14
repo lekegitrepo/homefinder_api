5bd65a8886ee9ddf9c9d1270984f3974db84c2fc
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 04:57:21 -0600

    create generate_token in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  before_create :generate_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :auth_token, uniqueness: true
+
+  def generate_token
+    loop do
+      self.auth_token = Devise.friendly_token
+      break unless self.class.exists?(auth_token: auth_token)
+    end
+  end
 end
