3fc1c22724c755c741fbf87400d3232d72efd36d
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 05:05:28 -0600

    import the authenticate file and add protect against forgery method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include JsonResponse
+  include Authenticate
+
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
