486c22b75fce97eeba139aa1fa89fafe71525b6e
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 03:41:27 -0600

    create the user registration create action method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {
+        msg: 'Successfully Registered',
+        user: user, is_success: true
+      }, status: :created, location: [:api_v1, user]
+    else
+      render json: {
+        msg: 'Unsuccessfully Registration',
+        errors: user.errors, is_success: false
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname, :username, :email, :password, :password_confirmation)
+  end
 end
