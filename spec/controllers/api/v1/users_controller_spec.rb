8f895ae2d8abb102531b242cb91e4bf7ad9bcb05
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 04:22:56 -0600

    add specs for invalid user registration

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -13,5 +13,26 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it { should respond_with 201 }
+
+    context 'When user provide invalid registration requirements' do
+      before(:each) do
+        @invalid_user = { password: '1234567', password_confirmation: '1234567' }
+        post :create, params: { user: @invalid_user, format: :json }
+      end
+
+      it 'should return a json with errors' do
+        expect(json_response[:is_success]).to eql false
+      end
+
+      it 'should contain json with errors key' do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "should contain an email error message: can't be blank" do
+        expect(json_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
   end
 end
