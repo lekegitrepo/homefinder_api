cb5dd3fbebad2845db3ba913ac9d9f02a433b325
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 02:08:13 -0600

    create the spec for user registration

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  
+  describe 'POST #create' do
+    before(:each) do
+      @user = FactoryBot.attributes_for :user
+      post :create, params: { user: @user, format: :json }
+    end
+
+    it 'should return the user record in json format' do
+      user_json = json_response[:user]
+      expect(user_json[:email]).to eql @user[:email]
+    end
+
+    it { should respond_with 201 }
+  end
 end
