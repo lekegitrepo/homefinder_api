0ed24cd9c142a4d4d1937af1916dd0ab61ca41b7
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 04:47:40 -0600

    add spec for the user show action method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -35,4 +35,18 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with 422 }
     end
   end
+
+  describe 'GET #show' do
+    before(:each) do
+      @user = FactoryBot.create :user
+      get :show, params: { id: @user.id, format: :json }
+    end
+
+    it 'should return an information about a user' do
+      user_resp = json_response[:user]
+      expect(user_resp[:email]).to eql @user.email
+    end
+
+    it { should respond_with 200 }
+  end
 end
