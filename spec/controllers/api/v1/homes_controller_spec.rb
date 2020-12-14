77d9bf909269edccc9a671e31144e7d083e0ffc6
lekegitrepo <lekedevrepo@gmail.com>
2020-12-14 11:15:39 -0600

    create spec for home controller

--- spec/controllers/api/v1/homes_controller_spec.rb
+++ spec/controllers/api/v1/homes_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::HomesController, type: :controller do
+  describe 'GET #show' do
+    before(:each) do
+      @home = FactoryBot.create :home
+      post :show, params: { id: @home.id, format: :json }
+    end
+
+    it 'should return information about the house' do
+      expect(json_response[:home]).to have_key(:type)
+    end
+  end
 end
