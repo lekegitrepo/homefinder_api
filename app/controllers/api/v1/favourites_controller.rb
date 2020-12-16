c473a4866e664b6260b52d6a53fb9d6e5a61967a
lekegitrepo <lekedevrepo@gmail.com>
2020-12-16 04:53:14 -0600

    implement the create action method

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::FavouritesController < ApplicationController
+  before_action :authenticate_with_token, only: [:create]
   def index
     user = User.find_by(id: params[:id])
     render json: user.homes.all
+  end
+
+  def create
+    home = Home.find_by(id: params[:id])
+    fav_home = Favourite.create(user_id: current_user.id, home_id: home.id)
+    home_json 'Set home as your favourite', true, fav_home, :created
   end
 end
