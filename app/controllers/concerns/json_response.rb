1a40af40bd310995c585ffc4372a1536f95d7a14
lekegitrepo <lekedevrepo@gmail.com>
2020-12-16 04:18:58 -0600

    create fav_json method

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -7,9 +7,17 @@ module JsonResponse
     }, status: status, location: loc
   end
 
-  def home_json(msg, success, user, status, loc = nil)
+  def home_json(msg, success, home, status, loc = nil)
     render json: {
-      home: user,
+      home: home,
+      message: msg,
+      is_success: success
+    }, status: status, location: loc
+  end
+
+  def fav_json(msg, success, fav, status, loc = nil)
+    render json: {
+      favourites: fav,
       message: msg,
       is_success: success
     }, status: status, location: loc
