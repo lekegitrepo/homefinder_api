ff39fae08643988166c08be852519c03e7f9a7a3
lekegitrepo <lekedevrepo@gmail.com>
2020-12-17 05:13:29 -0600

    set session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_local_trade', domain: 'https://homefinderapi.herokuapp.com/', same_site: :none, secure: true
+  Rails.application.config.session_store :cookie_store, key: '_homefinder', domain: 'https://homefinderapi.herokuapp.com/', same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_homefinder'
 end
