7250f7b6ef494fa41c62fd017545017c96562662
lekegitrepo <lekedevrepo@gmail.com>
2020-12-28 06:44:24 -0600

    create destroy action method

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::FavouritesController < ApplicationController
-  before_action :authenticate_with_token, only: %i[create index]
+  before_action :authenticate_with_token, only: %i[create index destroy]
   def index
     user = User.find_by(id: params[:id])
     render json: user.homes.all
@@ -9,5 +9,15 @@ class Api::V1::FavouritesController < ApplicationController
     home = Home.find_by(id: params[:id])
     fav_home = Favourite.create(user_id: current_user.id, home_id: home.id)
     fav_json 'Set home as your favourite', true, fav_home, :created
+  end
+
+  def destroy
+    fav = Favourite.find_by(id: params[:id])
+    if fav
+      fav.destroy
+      fav_json 'Successfully remove home from favourite', true, {}, :no_content
+    else
+      fav_json 'Error!', false, { errors: fav.errors }, :unprocessable_entity
+    end
   end
 end
