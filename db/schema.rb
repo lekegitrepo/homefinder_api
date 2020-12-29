68c0e7ccb2b1fe88442f994a81e63ee2c6a9279e
lekegitrepo <lekedevrepo@gmail.com>
2020-12-29 08:19:56 -0600

    migrate picke column to the homes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_173433) do
+ActiveRecord::Schema.define(version: 2020_12_29_141722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_12_14_173433) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_link"
     t.string "home_type"
+    t.boolean "picked"
   end
 
   create_table "users", force: :cascade do |t|
