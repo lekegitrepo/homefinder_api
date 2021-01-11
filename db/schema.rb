f1d64964fe884a7b49ec26dc6f101f45eb243627
lekegitrepo <lekedevrepo@gmail.com>
2021-01-11 04:46:54 -0600

    update the homes table with price column migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_141722) do
+ActiveRecord::Schema.define(version: 2021_01_11_104313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_141722) do
     t.string "image_link"
     t.string "home_type"
     t.boolean "picked"
+    t.float "price"
   end
 
   create_table "users", force: :cascade do |t|
