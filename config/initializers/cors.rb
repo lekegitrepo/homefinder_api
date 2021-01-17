a008820f734634b5af0fb6d2609094167bdc6a3a
lekegitrepo <lekedevrepo@gmail.com>
2021-01-17 07:43:50 -0600

    update the cors urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins 'https://homefinderapp.netlify.app'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
